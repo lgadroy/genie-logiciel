41bdaabfdbff00acfedbd197d34ef1565046961a
lgadroy <leo.gadroy@gmail.com>
2026-04-15 14:50:05 +0200

    Ajout du script de création de la bdd et d'une route de test.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -18,6 +19,19 @@ def gare():
 def chiffre():
     return render_template('chiffre.html')
 
+# Exemple de route pour tester la base de données
+@app.route('/testbdd')
+def test_bdd():
+    gares = test_get_gares()
+    return render_template('testbdd.html', gares=gares)
+
+def test_get_gares():
+    conn = sqlite3.connect('bdd/gares.db')
+    c = conn.cursor()
+    c.execute("SELECT ID_gare, Nom_gare FROM Gare")
+    gares = c.fetchall()
+    conn.close()
+    return gares
 
 if __name__ == '__main__':
     app.run(debug=True)
